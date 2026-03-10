46b3c09bfef8aafcf840c5974a8d3fb8cfb60cc5
Gergely Csécsey <gcsecsey@gmail.com>
2026-03-10 16:18:43 +0000

    Fix Android E2E tests in CI by removing invalid device profile and enabling KVM
    The 'Pixel 4' hardware profile was removed from newer Android SDK command-line
    tools, causing avdmanager to fail with "No device found matching --device Pixel 4".
    This prevented the AVD from being created and the emulator from booting, so Detox
    E2E tests couldn't run.
    
    Changes:
    - Remove 'profile: Pixel 4' from reactivecircus/android-emulator-runner config
    - Add KVM enablement step for hardware acceleration on ubuntu-latest
    - Rename AVD from 'Pixel_4_API_30' to 'test_emulator_api_30' in both CI and Detox config

--- .detoxrc.js
+++ .detoxrc.js
@@ -48,7 +48,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_4_API_30',
+        avdName: 'test_emulator_api_30',
       },
     },
   },
